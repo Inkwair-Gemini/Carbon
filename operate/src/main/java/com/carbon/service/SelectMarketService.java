a03269a883be91226e19a17ca6b464aaac6df7d7
xiangxiang_2003 <495296459@qq.com>
2023-12-24 18:36:10 +0800

    修改了SelectMarketService、SelectMarkServiceImpl注释位置

--- operate/src/main/java/com/carbon/service/SelectMarketService.java
+++ operate/src/main/java/com/carbon/service/SelectMarketService.java
@@ -4,19 +4,35 @@ import com.carbon.po.AuctionDoneRecord;
 import com.carbon.po.DirectionDoneRecord;
 import com.carbon.po.GroupDoneRecord;
 import com.carbon.po.ListingDoneRecord;
-import org.springframework.context.annotation.DependsOn;
+
 
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
 public interface SelectMarketService {
+
+    //查询当日单向竞价成交记录
     List<AuctionDoneRecord> selectAuctionDoneRecordCurrent();
+
+    //查询当日定向报价成交记录
     List<DirectionDoneRecord> selectDirectionDoneRecordCurrent();
+
+    //查询当日群组报价成交记录
     List<GroupDoneRecord> selectGroupDoneRecordCurrent();
+
+    //查询当日挂牌交易成交记录
     List<ListingDoneRecord> selectListingDoneRecordCurrent();
+
+    //查询历史单向竞价成交记录
     List<AuctionDoneRecord> selectAuctionDoneRecordHistory(LocalDate date);
+
+    //查询历史定向报价成交记录
     List<DirectionDoneRecord> selectDirectionDoneRecordHistory(LocalDate date);
+
+    //查询历史群组报价成交记录
     List<GroupDoneRecord> selectGroupDoneRecordHistory(LocalDate date);
+
+    //查询历史挂牌交易成交记录
     List<ListingDoneRecord> selectListingDoneRecordHistory(LocalDate date);
 }
