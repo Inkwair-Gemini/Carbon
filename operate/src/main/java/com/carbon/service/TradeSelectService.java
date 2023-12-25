d81572e0e96dc9804e4efa26db00a3bb341266f8
Inkwair-Gemini <Inkwair@126.com>
2023-12-25 10:02:04 +0800

    1.重新添加结果类 2.新建StringUtils 3.新增output类 4.合并挂牌表单与摘牌表单

--- operate/src/main/java/com/carbon/service/TradeSelectService.java
+++ operate/src/main/java/com/carbon/service/TradeSelectService.java
@@ -1,8 +1,31 @@
 package com.carbon.service;
 
+import com.carbon.input.AuctionRequest;
+import com.carbon.input.ListingPost;
+import com.carbon.output.DirectionAndGroupDoneRecordResult;
+import com.carbon.output.DirectionAndGroupEnquiryPostResult;
+import com.carbon.output.DirectionAndGroupPostResult;
+import com.carbon.po.AuctionDoneRecord;
+import com.carbon.po.ListingDoneRecord;
+import java.util.Date;
+import java.util.List;
+
 public interface TradeSelectService {
-    //查询当日交易
-    List selectTodayTrade();
-    //查询历史交易
-    List selectHistoryTrade();
+    //挂牌交易历史委托 标的物 日期 方向
+    List<ListingPost> selectListingPost(String operatorCode, String subjectMatterCode, Date beginTime, Date endTime, String flowType);
+    //挂牌交易历史成交
+    List<ListingDoneRecord> selectListingDoneRecord(String clientId, String subjectMatterCode, Date beginTime, Date endTime, String flowType);
+    //大宗协议交易报价查询
+    List<DirectionAndGroupPostResult> selectDirectionAndGroupPost(String clientId, String subjectMatterCode, Date beginTime, Date endTime, String flowType);
+    //大宗协议交易询价查询
+    List<DirectionAndGroupEnquiryPostResult> selectDirectionAndGroupEnquiryPost(String clientId, String subjectMatterCode, Date beginTime, Date endTime, String flowType);
+
+    //大宗协议交易对话流水查询 //todo 准备阉了
+
+    //大宗协议交易询价查询
+    List<DirectionAndGroupDoneRecordResult> selectDirectionAndGroupDoneRecord(String clientId, String subjectMatterCode, Date beginTime, Date endTime, String flowType);
+    //单向竞价交易请求查询
+    List<AuctionRequest> selectAuctionRequest(String clientId,String subjectMatterCode, Date beginTime, Date endTime);
+    //单向竞价交易成交查询
+    List<AuctionDoneRecord> selectAuctionDoneRecord(String clientId,String subjectMatterCode, Date beginTime, Date endTime);
 }
