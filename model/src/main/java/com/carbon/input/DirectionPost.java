0d3e59f8962a446d0b7e9bb90f999ca873bc47aa
xiangxiang_2003 <495296459@qq.com>
2024-01-04 00:23:02 +0800

    修改两个post中的quotaAccount为account

--- model/src/main/java/com/carbon/input/DirectionPost.java
+++ model/src/main/java/com/carbon/input/DirectionPost.java
@@ -18,7 +18,7 @@ public class DirectionPost implements Serializable {
     private String subjectMatterCode; // 标的物代码
     private String subjectMatterName; // 标的物名称
     private String accountType; // 账户类型
-    private String quotaAccount; // 配额账户
+    private String account; // 账户
     private String flowType; // 买入/卖出
     private Double price; // 委托价格
     private Double amount; // 委托数量
