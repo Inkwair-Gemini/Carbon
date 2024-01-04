450898d08c6846e31fb64b7f0e5d43f3f7977e64
Inkwair-Gemini <Inkwair@126.com>
2024-01-04 19:18:27 +0800

    1.更正客户交易配额的主键AccountId->quotaAccountId 2.引入springboot——web 3.完善配额服务类 4.完成配额管理控制层

--- model/src/main/java/com/carbon/po/ClientTradeQuota.java
+++ model/src/main/java/com/carbon/po/ClientTradeQuota.java
@@ -4,13 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 
 // 客户交易配额
 public class ClientTradeQuota implements Serializable {
-    private String AccountId; // 配额账户Id
+    private String quotaAccountId; // 配额账户Id
     private String subjectMatterCode; // 标的物代码
     private String subjectMatterName; // 标的物名称
     private Double amount; // 总数量
