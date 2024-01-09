4b0447795879edf1e653a47534515aa2f5f33407
alomsten <1194972692@qq.com>
2024-01-09 21:22:43 +0800

    修改单向竞价委托单属性，新增最新价格

--- model/src/main/java/com/carbon/input/AuctionRequest.java
+++ model/src/main/java/com/carbon/input/AuctionRequest.java
@@ -22,6 +22,7 @@ public class AuctionRequest implements Serializable {
     private String quotaAccount; // 配额账户
     private Double price; // 委托价格
     private Double amount; // 委托数量
+    private Double recentPrice; //最新价格
     private String operatorCode; // 操作员代码
     private String status; // 状态
 }
