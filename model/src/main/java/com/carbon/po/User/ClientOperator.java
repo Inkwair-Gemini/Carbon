41528f0438de2a2426c5f854de1cb721cfaa82c4
Inkwair-Gemini <Inkwair@126.com>
2024-01-10 23:32:43 +0800

    修复跨域

--- model/src/main/java/com/carbon/po/User/ClientOperator.java
+++ model/src/main/java/com/carbon/po/User/ClientOperator.java
@@ -1,9 +1,8 @@
 package com.carbon.po.User;
-
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.io.Serializable;
 
 @Data
@@ -12,6 +11,7 @@ import java.io.Serializable;
 
 // 客户操作员
 public class ClientOperator implements Serializable {
+    @JsonProperty(value = "operatorCode")
     private String id; // 操作员Id
     private String clientId; // 客户Id
     private String password; // 密码
